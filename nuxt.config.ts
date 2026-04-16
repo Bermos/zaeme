6b27a1d3210b5d8a1c076de502dc4a96e9ba2a38
Bermos <gh@bermos.dev>
2026-04-16 10:37:07 +0200

    Initial nuxt setup

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/a11y',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/i18n',
+    '@nuxtjs/mcp-toolkit',
+    'nuxt-posthog'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
