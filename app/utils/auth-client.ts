d774e9b31f4b54b5828b3a8feb389372e744001a
Bermos <accounts@machivenyika.ch>
2026-04-22 18:30:12 +0200

    fix: auth-client baseURL
    removed baseURL setting from better-auth auth-client as we use the same domain for front and back end anyways.

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -3,9 +3,6 @@ import { magicLinkClient } from 'better-auth/client/plugins'
 // import { passkeyClient } from "@better-auth/passkey/client";
 
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
-
   plugins: [
     magicLinkClient()
     // passkeyClient()
